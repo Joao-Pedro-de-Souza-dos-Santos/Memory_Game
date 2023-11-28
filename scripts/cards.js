d162df0b67b0d448269a683e2dd04d1990d30754
Joao-Pedro-de-Souza-dos-Santos <joaopedrodesouzadossantos15@gmail.com>
2023-11-27 21:17:18 -0300

    add logic cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -4,7 +4,22 @@ function backPage() {
         window.history.back();// 👈👌 Código TopZeira autofocus //
     }
 }
+
 function createCards() {
+    const cardNames = [
+    "card-1",
+    "card-2",
+    "card-3",
+    "card-4",
+    "card-5",
+    "card-6",
+    "card-7",
+    ];
+
+    const arrayCardsName = cardNames
+        .sort(() => Math.random() - 0.5)
+        .filter((value, index) => index < 7);
+    const sortedCards = [...arrayCardsName,...arrayCardsName].sort(() => Math.random() - 0.5);
     gridCards.innerHTML = "";
     sortedCards.forEach((card) => {
         gridCards.innerHTML += `
@@ -20,31 +35,56 @@ function createCards() {
     });
 }
 
+function clickFlipCard() {
+    const arrayCards = document.querySelectorAll(".card");
+    arrayCards.forEach((card) => {
+        card.addEventListener("click", () => {
+            console.log("Da uma apertadinha aq no pai");
+            if (card.classList.contains("flipCard")) return;
+            new Audio("../audios/flip.wav").play();
+            if (firstCard === "") {
+                card.classList.add("flipCard");
+                firstCard = card;
+            } else if (secondCard === "") {
+                card.classList.add("flipCard");
+                secondCard = card;
+
+                checkMatchCards();
+            }   
+        })
+    })
+
+}
+
+function checkMatchCards() {
+    if (firstCard.getAttribute("name") === secondCard.getAttribute("name")) {
+        new Audio("../audios/sci-fi.wav").play();
+        setTimeout(() => {
+            firstCard.classList.add("disabledCard");
+            secondCard.classList.add("disabledCard");
+            firstCard = ""; secondCard = "";
+        }, 500)
+        
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove("flipCard");
+            secondCard.classList.remove("flipCard");
+            firstCard = ""; secondCard = "";
+        }, 500)
+        
+    }
+}
+
 const playerNick = document.querySelector(".playerNick");
 const backButton = document.querySelector(".backButton");
 const gridCards = document.querySelector(".gridCards");
-
 
 const storagePlayerNick = localStorage.getItem("@memoryGame:playerName");
 playerNick.innerHTML = storagePlayerNick;
 backButton.addEventListener("click", backPage);
 
-const cardNames = [
-    "card-1",
-    "card-2",
-    "card-3",
-    "card-4",
-    "card-5",
-    "card-6",
-    "card-7",
-];
-
-const arrayCardsName = cardNames
-    .sort(() => Math.random() - 0.5)
-    .filter((value, index) => index < 7);
-const sortedCards = [...arrayCardsName,...arrayCardsName].sort(() => Math.random() - 0.5);
-
 createCards();
 
-console.table(arrayCardsName);
-console.table(sortedCards);
+let firstCard = "";
+let secondCard = "";
+clickFlipCard();
